a9b58b6d47e7ca79be254bcd483d697f3690e3de
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-02-22 12:30:26 -0800

    modified: public/index.html modified:   src/components/Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,22 +3,13 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import styled from 'styled-components';
 import logo from '../e-commerce_logo.svg';
-const Img = styled.img`
-  width: 15.5rem;
-  margin-right:5px;
-
-  @media (max-width: 376px) {
-    width:11rem;
-  }
-`;
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
       <Container>
-      <Img src={logo} />
         <LinkContainer to='/'>
-          <Navbar.Brand >F.A.B.E Hockey Shop</Navbar.Brand>
+          <Img src={logo} />
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -32,7 +23,17 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
+
+//Styles 
+const Img = styled.img`
+  width: 15.5rem;
+  margin-right:5px;
+
+  @media (max-width: 376px) {
+    width:11rem;
+  }
+`;
 
 
 

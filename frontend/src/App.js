4fbd685ee2aa93ff2e0ec31c1d819443d7fa4000
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-02-22 11:54:30 -0800

    new file:   build/FAVE_LOGO.png

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,45 @@
-import React from 'react'
-
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
 function App() {
   return (
     <>
-     <h1>Welcome to F.A.B.E Hockey Online Shop</h1>
+      <GlobalStyle />
+      <Router>
+        <Header />
+        <Main className="py-3">
+          <Container>
+            <Route path='/' component={HomeScreen} exact/>
+            <Route path='/product/:id' component={ProductScreen}/>
+          </Container>
+        </Main>
+        <Footer />
+      </Router>
     </>
   );
 }
 
 export default App;
+
+// styled components
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+  h3{
+    padding : 1rem 0;
+  }
+`;
+
+const Main = styled.main`
+  min-height: 80vh
+`;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import logo from '../e-commerce_logo.svg';
+const Img = styled.img`
+  width: 15rem;
+  margin-right:5px;
+
+  @media (max-width: 376px) {
+    width:11rem;
+  }
+`;
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+      <Container>
+      <Img src={logo} />
+        <LinkContainer to='/'>
+          <Navbar.Brand >F.A.B.E Hockey Shop</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+            <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
+
+
+
+
